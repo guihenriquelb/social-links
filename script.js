03cd28b497670d1b0ed86ce6842761fe2800f2a1
Guilherme Henrique <guihenrique011@hotmail.com>
2023-08-09 20:28:27 -0300

    dark mode persistence

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
-function toggleMode() {
-  const html = document.documentElement
+const html = document.documentElement
+let lightMode = localStorage.getItem("light-mode")
+
+
+
+function enableLightMode() {
   html.classList.toggle("light")
-
-  // pegar a tag img
-  const img = document.querySelector("#profile img")
-
-  //substituir a imagem
-  if (html.classList.contains("light")) {
-    // se tiver lightmode, adicionar light
-    img.setAttribute("src", "./assets/avatar-light.png")
-  } else {
-    // se tiver sem light, manter
-    img.setAttribute("src", "./assets/avatar.png")
-  }
+  localStorage.setItem("light-mode", "enabled")
 }
+
+function disableLightMode() {
+  html.classList.toggle("light")
+  localStorage.setItem("light-mode", "disabled")
+}
+
+if (lightMode === "enabled") {
+  enableLightMode()
+}
+
+
+function toggleMode() {
+ 
+  if (lightMode === "disabled") {
+    enableLightMode()
+  } else {
+    disableLightMode()
+  }
+
+ 
+
+
+
+}
+
+
